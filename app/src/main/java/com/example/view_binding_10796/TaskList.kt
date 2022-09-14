00ddf298819028a9d49c8f57eed1fc695a6f573c
EricDavinsen <edavinsen@gmail.com>
2022-09-14 14:05:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10796/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10796
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Tutor Spiral Abyss", "Showcase"),
+        Task("Chapter 3", "Turu dek", "Besok nguli"),
+        Task("Chapter 4", "GG Gaming", "Adik-adik"),
+        Task("Chapter 5", "Hari hari ngetroll", "NT Kadang-kadang")
+    )
+}
